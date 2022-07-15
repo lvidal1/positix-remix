eb4fe47bb1d7ad9c8c771725014bfb2c12d6e167
lvidal1 <lvidal910@gmail.com>
2022-07-15 00:43:06 -0500

    [concept/routes] Add markdown deps & render html

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,3 +1,4 @@
+import { marked } from "marked";
 import type { LoaderFunction } from "@remix-run/node"
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
@@ -6,18 +7,20 @@ import invariant from "tiny-invariant";
 import type { Post } from "~/models/post.server";
 import { getPost } from "../../models/post.server";
 
-type LoaderData = { post: Post };
+type LoaderData = { post: Post, html: string };
 
 
 export const loader: LoaderFunction = async ({ params }) => {
     const post = await getPost(params.slug);
     invariant(post, `Post not found: ${params.slug}`);
 
-    return json<LoaderData>({ post });
+    const html = marked(post.markdown);
+
+    return json<LoaderData>({ post, html });
 }
 
 export default function PostSlug() {
-    const { post } = useLoaderData() as LoaderData;
+    const { post, html } = useLoaderData() as LoaderData;
 
     console.log(post);
     return (
@@ -25,9 +28,7 @@ export default function PostSlug() {
             <h1 className="my-6 border-b-2 text-center text-3xl">
                 {post.title}
             </h1>
-            <pre>
-                {post.markdown}
-            </pre>
+            <div dangerouslySetInnerHTML={{ __html: html }} />
         </main>
     )
 }
